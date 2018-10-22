76102d2b8028f49362f09a299d3590bcc704f01a
Yoshiyuki Takeda <andysumi@ruby.net.tokai-u.jp>
2018-10-22 09:08:32 +0900

    ESLintの導入

--- code.js
+++ code.js
@@ -1,3 +1,3 @@
-function doGet(e){
+function doGet(e) { // eslint-disable-line no-unused-vars
   return ContentService.createTextOutput('Hello, world!!\nHello, clasp!!\nHello, Bitbucket Pipelines!!');
 }
